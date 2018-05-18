1be1be7766389f3062aaadb56c4308c33414335e
feripratama <eroorsys@gmail.com>
2018-05-18 19:02:13 +0700

    Update model, route, migration
     On branch master
     Changes to be committed:
    	modified:   src/Http/Controllers/TransaksiController.php
    	modified:   src/Models/TransaksiModel.php
    	modified:   src/database/migrations/2018_05_17_144444_create_transaksi_table.php
    	modified:   src/routes/routes.php

--- src/database/migrations/2018_05_17_144444_create_transaksi_table.php
+++ src/database/migrations/2018_05_17_144444_create_transaksi_table.php
@@ -13,9 +13,22 @@ class CreateTransaksiTable extends Migration
      */
     public function up()
     {
-        Schema::create('transaksi', function (Blueprint $table) {
+        Schema::create('transaksies', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+			$table->uuid('uuid', 191)->unique();
+			$table->string('nomor', 191)->unique();
+			$table->double('total');
+			$table->double('grandtotal')->nullable();
+			$table->double('denda')->default('0.00');
+			$table->double('potongan');
+			$table->integer('admin_id')->unsigned()->nullable()->index();
+			$table->integer('customer_transaksi_id')->unsigned()->nullable()->index();
+			$table->integer('tarif_id')->unsigned()->nullable();
+			$table->uuid('admin_uuid', 191)->index();
+			$table->uuid('customer_transaksi_uuid', 191)->index();
+			$table->uuid('tarif_uuid')->index();
+			$table->timestamps();
+			$table->softDeletes();
         });
     }
 
@@ -26,6 +39,6 @@ class CreateTransaksiTable extends Migration
      */
     public function down()
     {
-        Schema::drop('transaksi');
+        Schema::drop('transaksies');
     }
 }
